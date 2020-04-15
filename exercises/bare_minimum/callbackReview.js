743af509a8abc720131a4e023f6bddeceef05104
Justin Paoletta <justinpaoletta@gmail.com>
2020-04-15 02:26:49 -0400

    promises half way complete

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,32 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  // Read the File  (fs.readFile())
+  fs.readFile(filePath, 'utf8', (err, file) => {
+    // if error run callback with the err and file
+    if (err) {
+      callback(err, file);
+    } else {
+      // if no error split the file by line and grab the first one
+      file = file.split('\n')[0];
+      // run callback with null since no error and the file
+      callback(null, file);
+    }
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, (error, res) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -8,14 +8,46 @@ var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
 
+
+// PROMISE PATTERN
+
+// new Promise
+// do something async
+// Pass the successful value into the resolve function
+// this value will be made available in the next then block
+// only 1 value can ever be passed into resolve
+
+// Pass any errors into the reject function
+// this error will be made available in the catch block
+// return the promise instance. This should be a synchronous step
+
+
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, file) => {
+      if (err) {
+        reject(err);
+      } else {
+        file = file.toString().split('\n')[0];
+        resolve(file);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    request(url, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
